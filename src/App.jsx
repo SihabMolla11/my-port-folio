b1b2254d1a7c27e463172e124109ddea1f0726bc
unknown <sihabuddinmolla11@gmail.com>
2023-06-26 00:31:50 +0600

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./Components/Main/Main";
+
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <div>
+      <div></div>
+      <div>Body</div>
+      <div>Footer</div>
+    </div>
+  );
+};
+
+export default Main;
