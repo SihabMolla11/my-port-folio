a8e37906ad78005270c08cf050c315592783238d
SihabMolla11 <sihabuddinmolla11@gmail.com>
2024-08-18 23:51:43 +0600

    project section done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,35 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { projectData } from "../../data/projectData";
 import Project from "./Project";
+import ProjectCart from "./ProjectCart";
 import Modal from "./ProjectDetail/Modal";
 
 const Projects = () => {
-    const [projects, setProjects] = useState([]);
-    const [uniqueId, setUniqueId] = useState(null);
-    //   console.log(uniqueId);
+  const [uniqueId, setUniqueId] = useState(null);
 
-    useEffect(() => {
-        fetch("./data.json")
-            .then((res) => res.json())
-            .then((data) => setProjects(data));
-    }, []);
-
-    return (
-        <div data-aos="fade-down" className="my-container mt-20">
-            <h2 className="text-4xl text-center font-bold text-white  uppercase">
-                PROJECTS
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-16">
-                {projects?.map((project) => (
-                    <Project
-                        project={project}
-                        key={project.id}
-                        setUniqueId={setUniqueId}
-                    />
-                ))}
-            </div>
-            <Modal uniqueId={uniqueId} />
-        </div>
-    );
+  return (
+    <div data-aos="fade-down" className="my-container mt-20">
+      <h2 className="text-4xl text-center font-bold text-white  uppercase">
+        PROJECTS
+      </h2>
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-16">
+        {projectData?.map((project) => (
+          <Project
+            project={project}
+            key={project.id}
+            setUniqueId={setUniqueId}
+          />
+        ))}
+      </div> */}
+      <div className="space-y-6 mt-8">
+        {projectData?.map((data) => (
+          <ProjectCart key={data?.id} data={data} />
+        ))}
+      </div>
+      <Modal uniqueId={uniqueId} />
+    </div>
+  );
 };
 
 export default Projects;
---
+++ src/data/projectData.js
@@ -0,0 +1,387 @@
+export const projectData = [
+  {
+    id: 7,
+    name: "SATUJOBS",
+    liveSiteLink: "https://satujobs.com",
+    image: "/image/project_image/satujobs.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "/image/skills/tailwind_css.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "/image/skills/bootstrap.png",
+      },
+      {
+        name: "Typescript",
+        image: "/image/skills/typescript.png",
+      },
+      {
+        name: "Next Js",
+        image: "/image/skills/nextjs.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Redux Js",
+        image: "/image/skills/redux.png",
+      },
+      {
+        name: "React Query",
+        image: "/image/skills/react-query.png",
+      },
+      {
+        name: "React Hook Form",
+        image: "/image/skills/react-hook-form.png",
+      },
+      {
+        name: "Socket Io",
+        image: "/image/skills/socket_io.webp",
+      },
+      {
+        name: "NestJs",
+        image: "/image/skills/nestjs.png",
+      },
+      {
+        name: "Prisma",
+        image: "/image/skills/prisma.png",
+      },
+      {
+        name: "PostgresSQL",
+        image: "/image/skills/postgresql.png",
+      },
+      {
+        name: "Node Js",
+        image: "/image/skills/node-js.png",
+      },
+      {
+        name: "Express Js",
+        image: "/image/skills/express.png",
+      },
+      {
+        name: "Mongoose",
+        image: "/image/skills/mongoose.png",
+      },
+      {
+        name: "MongoDB",
+        image: "/image/skills/mongodb.png",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "ESPD School",
+    liveSiteLink: "https://espd.school",
+    image: "/image/project_image/espd_school.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "/image/skills/bootstrap.png",
+      },
+      {
+        name: "Typescript",
+        image: "/image/skills/typescript.png",
+      },
+      {
+        name: "Next Js",
+        image: "/image/skills/nextjs.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Redux Js",
+        image: "/image/skills/redux.png",
+      },
+      {
+        name: "React Query",
+        image: "/image/skills/react-query.png",
+      },
+      {
+        name: "React Hook Form",
+        image: "/image/skills/react-hook-form.png",
+      },
+      {
+        name: "NestJs",
+        image: "/image/skills/nestjs.png",
+      },
+      {
+        name: "Prisma",
+        image: "/image/skills/prisma.png",
+      },
+      {
+        name: "PostgresSQL",
+        image: "/image/skills/postgresql.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Senior Places",
+    liveSiteLink: "https://seniorplaces.org",
+    image: "/image/project_image/senior_places.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "/image/skills/bootstrap.png",
+      },
+      {
+        name: "Typescript",
+        image: "/image/skills/typescript.png",
+      },
+      {
+        name: "Next Js",
+        image: "/image/skills/nextjs.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Redux Js",
+        image: "/image/skills/redux.png",
+      },
+      {
+        name: "React Query",
+        image: "/image/skills/react-query.png",
+      },
+      {
+        name: "React Hook Form",
+        image: "/image/skills/react-hook-form.png",
+      },
+      {
+        name: "NestJs",
+        image: "/image/skills/nestjs.png",
+      },
+      {
+        name: "Prisma",
+        image: "/image/skills/prisma.png",
+      },
+      {
+        name: "PostgresSQL",
+        image: "/image/skills/postgresql.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Medicare Poing",
+    liveSiteLink: "https://medicare-point-1bbbf.web.app",
+    clientCode: "https://github.com/atikur-24/medicare-point-client",
+    serverCodeLink: "https://github.com/atikur-24/medicare-point-server",
+    image: "/image/project_image/medicare_point.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "/image/skills/tailwind_css.png",
+      },
+      {
+        name: "Javascript",
+        image: "/image/skills/js.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Redux Js",
+        image: "/image/skills/redux.png",
+      },
+      {
+        name: "React Query",
+        image: "/image/skills/react-query.png",
+      },
+      {
+        name: "Firebase",
+        image: "/image/skills/firebase.png",
+      },
+      {
+        name: "Node js",
+        image: "/image/skills/node-js.png",
+      },
+      {
+        name: "Express js",
+        image: "/image/skills/express.png",
+      },
+      {
+        name: "Mongodb",
+        image: "/image/skills/mongodb.png",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Photo Graph House",
+    liveSiteLink:
+      "https://648a065489800f0c29a0440d--mellifluous-sorbet-0711fd.netlify.app",
+    clientCode: "https://github.com/SihabMolla11/summer-camp-client",
+    serverCodeLink: "https://github.com/SihabMolla11/summer-camp-sarver",
+    image: "/image/project_image/summer_camp_main.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "/image/skills/tailwind_css.png",
+      },
+      {
+        name: "Javascript",
+        image: "/image/skills/js.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Firebase",
+        image: "/image/skills/firebase.png",
+      },
+      {
+        name: "Node js",
+        image: "/image/skills/node-js.png",
+      },
+      {
+        name: "Express js",
+        image: "/image/skills/express.png",
+      },
+      {
+        name: "Mongodb",
+        image: "/image/skills/mongodb.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Gem Toys Mart",
+    liveSiteLink: "https://assignment-11-5d73f.web.app",
+    clientCode: "https://github.com/SihabMolla11/toys-mart-client",
+    serverCodeLink: "https://github.com/SihabMolla11/toys-mart-sarver",
+    image: "/image/project_image/toys_mart.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "/image/skills/tailwind_css.png",
+      },
+      {
+        name: "Javascript",
+        image: "/image/skills/js.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Firebase",
+        image: "/image/skills/firebase.png",
+      },
+      {
+        name: "Node js",
+        image: "/image/skills/node-js.png",
+      },
+      {
+        name: "Express js",
+        image: "/image/skills/express.png",
+      },
+      {
+        name: "Mongodb",
+        image: "/image/skills/mongodb.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Chefs Hunter",
+    liveSiteLink: "https://friendly-basbousa-200b85.netlify.app",
+    clientCode: "https://github.com/SihabMolla11/chefs-hounter-client",
+    serverCodeLink: "https://github.com/SihabMolla11/cher-hunter-sarver",
+    image: "/image/project_image/chefs_hunter.png",
+    technology: [
+      {
+        name: "HTML",
+        image: "/image/skills/html.png",
+      },
+      {
+        name: "CSS",
+        image: "/image/skills/css.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "/image/skills/tailwind_css.png",
+      },
+      {
+        name: "Javascript",
+        image: "/image/skills/js.png",
+      },
+      {
+        name: "React Js",
+        image: "/image/skills/react.png",
+      },
+      {
+        name: "Firebase",
+        image: "/image/skills/firebase.png",
+      },
+      {
+        name: "Node js",
+        image: "/image/skills/node-js.png",
+      },
+      {
+        name: "Express js",
+        image: "/image/skills/express.png",
+      },
+      {
+        name: "Mongodb",
+        image: "/image/skills/mongodb.png",
+      },
+    ],
+  },
+];
